61d32cb39d07597ba8b04a5e3887a6b8c4689143
DWT <AlekseyAnalytics@yandex.ru>
2020-12-02 09:35:05 +0300

    logo

--- src/test-style/index.tsx
+++ src/test-style/index.tsx
@@ -3,13 +3,11 @@ import React from "react";
 import { HTMLLogo } from "./logo.style";
 import { HTMLContainer, HTMLImage } from "./style";
 
-import Logo from "./logo.jpeg";
-
 export const TestStyle: React.FC<{}> = () => {
   return (
     <HTMLContainer>
       <p>Необходимо средствами css реализовать логотип указанный ниже:</p>
-      <HTMLImage src={Logo} />
+      <HTMLImage src="https://github.com/Diamis/frontend-test-ts/blob/main/public/logo.jpeg?raw=true" />
       <br />
       <p>Логотип</p>
       <HTMLLogo />
